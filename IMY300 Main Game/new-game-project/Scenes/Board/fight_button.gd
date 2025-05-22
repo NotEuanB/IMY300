6ab89b8e07cfe26851d303bcc8e75f0525b6485e
Nolan <142321456+nolzftw@users.noreply.github.com>
2025-05-22 17:57:34 +0200

    Sync

--- IMY300 Main Game/new-game-project/Scenes/Board/fight_button.gd
+++ IMY300 Main Game/new-game-project/Scenes/Board/fight_button.gd
@@ -12,5 +12,3 @@ func _on_mouse_exited() -> void:
 
 func _on_pressed() -> void:
 	$FightSprite/FightAnimation.play("Fight_press")
-	await get_tree().create_timer(0.5).timeout  
-	emit_signal("mouse_entered")  
