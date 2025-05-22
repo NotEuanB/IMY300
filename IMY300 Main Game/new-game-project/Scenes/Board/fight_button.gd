f5fc1e2f63fe014707929d9f26a02a4723eafe62
Nolan <142321456+nolzftw@users.noreply.github.com>
2025-05-22 17:47:03 +0200

    Fight press also added

--- IMY300 Main Game/new-game-project/Scenes/Board/fight_button.gd
+++ IMY300 Main Game/new-game-project/Scenes/Board/fight_button.gd
@@ -9,3 +9,8 @@ func _on_mouse_entered() -> void:
 
 func _on_mouse_exited() -> void:
 	$FightSprite/FightAnimation.play("Fight_idle")
+
+func _on_pressed() -> void:
+	$FightSprite/FightAnimation.play("Fight_press")
+	await get_tree().create_timer(0.5).timeout  
+	emit_signal("mouse_entered")  
