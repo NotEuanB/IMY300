1cb8593ab0444bdc0b6561e92b8074c8874b7851
Nolan <142321456+nolzftw@users.noreply.github.com>
2025-05-24 14:43:55 +0200

    Added E_Worm

--- IMY300 Main Game/new-game-project/Scenes/CombinationPage/combination_page.gd
+++
@@ -1,7 +0,0 @@
-class_name CombinationPage
-extends Control
-
-func _on_decline_button_pressed() -> void:
-	self.get_parent().visible = false
-	get_parent().get_parent().get_node("ShopUI").visible = true
-	
