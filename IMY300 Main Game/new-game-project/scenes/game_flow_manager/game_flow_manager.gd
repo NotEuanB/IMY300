d9a85041b9c5242d350dbbba40e22ccf2802eed4
Nolan <142321456+nolzftw@users.noreply.github.com>
2025-07-26 15:07:58 +0200

    Sync

--- IMY300 Main Game/new-game-project/scenes/game_flow_manager/game_flow_manager.gd
+++ IMY300 Main Game/new-game-project/scenes/game_flow_manager/game_flow_manager.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var unit_mover: UnitMover = $UnitMover
 
 func _ready() -> void:
+	$FlowMenuAnimation/FlowMenuAnimation.play("Flowmenu_idle")
 	_update_ui()
 	var states = GameState.load_state()
 	var board_units = states["board_units"]
@@ -24,34 +25,37 @@ func _spawn_units(units_data: Array, play_area: PlayArea) -> void:
 
 func _update_ui() -> void:
 	# Hide all buttons initially
-	$VBoxContainer/ShopButton.visible = false
-	$VBoxContainer/CombineButton.visible = false
-	$VBoxContainer/FightButton.visible = false
+	$ShopButton.visible = false
+	$CombineButton.visible = false
+	$FightButton.visible = false
 
 	# Show buttons based on the current step
 	match GameState.current_step:
 		GameState.GameStep.STEP_1, GameState.GameStep.STEP_2:
-			$VBoxContainer/ShopButton.visible = true
-			$VBoxContainer/CombineButton.visible = true
+			$ShopButton.visible = true
+			$CombineButton.visible = true
 		GameState.GameStep.FIGHT:
-			$VBoxContainer/ShopButton.visible = false
-			$VBoxContainer/CombineButton.visible = false
-			$VBoxContainer/FightButton.visible = true
-
-func _on_ShopButton_pressed() -> void:
-	# Load the Shop scene
-	_load_scene("res://Scenes/Board/board.tscn")
-
-func _on_CombineButton_pressed() -> void:
-	# Load the Combine scene
-	_load_scene("res://Scenes/combine_board/combineboard.tscn")
-
-func _on_FightButton_pressed() -> void:
-	# Load the Fight scene
-	_load_scene("res://Scenes/forest_board/forestboard.tscn")
+			$ShopButton.visible = false
+			$CombineButton.visible = false
+			$FightButton.visible = true
 
 func _load_scene(scene_path: String) -> void:
 	print("Changing scene to:", scene_path)
 	GameState.update_step()  # Centralized game step update
 	get_tree().change_scene_to_file(scene_path)
 	_update_ui()
+
+
+func _on_fight_button_pressed() -> void:
+	# Load the Fight scene
+	_load_scene("res://Scenes/forest_board/forestboard.tscn")
+
+
+func _on_combine_button_pressed() -> void:
+	# Load the Combine scene
+	_load_scene("res://Scenes/combine_board/combineboard.tscn")
+
+
+func _on_shop_button_pressed() -> void:
+	# Load the Shop scene
+	_load_scene("res://Scenes/Board/board.tscn")
--- IMY300 Main Game/new-game-project/scenes/pause_menu/pause_menu.gd
+++ IMY300 Main Game/new-game-project/scenes/pause_menu/pause_menu.gd
@@ -9,5 +9,6 @@ func _on_resume_pressed() -> void:
 func _on_quit_pressed() -> void:
 	player_stats.reset_stats()
 	GameState.reset_state()
+	GameState.reset_step()
 	Engine.time_scale = 1
 	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
