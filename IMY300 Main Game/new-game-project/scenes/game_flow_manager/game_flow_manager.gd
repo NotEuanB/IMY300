f2aed48da9d9197d3a54a9b869738ffaa59ceb81
Nolan <142321456+nolzftw@users.noreply.github.com>
2025-07-26 15:13:53 +0200

    Revert "Merge branch 'Euan' into Nolz"
    This reverts commit 5ad9c92684fbeefb9982f04f48530fe058978acf, reversing
    changes made to d9a85041b9c5242d350dbbba40e22ccf2802eed4.

--- IMY300 Main Game/new-game-project/scenes/game_flow_manager/game_flow_manager.gd
+++ IMY300 Main Game/new-game-project/scenes/game_flow_manager/game_flow_manager.gd
@@ -1,57 +1,61 @@
 extends Control
 
-@onready var tutorial_popup = $TutorialPopup
-@onready var tutorial_text = $TutorialPopup/TutorialText
+@onready var unit_mover: UnitMover = $UnitMover
 
 func _ready() -> void:
+	$FlowMenuAnimation/FlowMenuAnimation.play("Flowmenu_idle")
 	_update_ui()
-	_show_tutorial_popup()
+	var states = GameState.load_state()
+	var board_units = states["board_units"]
+	var hand_units = states["hand_units"]
+	_spawn_units(board_units, $PlayArea)
+	_spawn_units(hand_units, $HandArea)
 
-func _show_tutorial_popup() -> void:
-	match GameState.current_step:
-		GameState.GameStep.STEP_1:
-			tutorial_text.text = "Welcome to the game! Here are your options:\n\n- Shop: Buy units.\n- Combine: Combine units.\n- Fight: Start a battle.\n\nPress the Shop button to begin."
-			$TutorialPopup.visible = true  # Show the tutorial panel
-		GameState.GameStep.STEP_2:
-			tutorial_text.text = "Now that you know the shop, let's move on to the combination.\n\n Press on the Combine button to continue."
-			$TutorialPopup.visible = true  # Show the tutorial panel
+func _spawn_units(units_data: Array, play_area: PlayArea) -> void:
+	for unit_data in units_data:
+		var stats = unit_data["stats"]
+		var tile = unit_data["tile"]
+		var unit_scene = stats.unit_scene if stats.unit_scene else preload("res://scenes/unit/unit.tscn")
+		var unit = unit_scene.instantiate()
+		play_area.unit_grid.add_child(unit)
+		play_area.unit_grid.add_unit(tile, unit)
+		unit.global_position = play_area.get_global_from_tile(tile)
+		unit.stats = stats.duplicate()
+		unit_mover.setup_unit(unit)
 
 func _update_ui() -> void:
 	# Hide all buttons initially
-	$VBoxContainer/ShopButton.visible = false
-	$VBoxContainer/CombineButton.visible = false
-	$VBoxContainer/FightButton.visible = false
+	$ShopButton.visible = false
+	$CombineButton.visible = false
+	$FightButton.visible = false
 
 	# Show buttons based on the current step
 	match GameState.current_step:
-		GameState.GameStep.STEP_1:
-			$VBoxContainer/ShopButton.visible = true
-			$VBoxContainer/CombineButton.visible = true
-			$VBoxContainer/CombineButton.disabled = true
-		GameState.GameStep.STEP_2:
-			$VBoxContainer/ShopButton.disabled = true
-			$VBoxContainer/ShopButton.visible = true
-			$VBoxContainer/CombineButton.visible = true
+		GameState.GameStep.STEP_1, GameState.GameStep.STEP_2:
+			$ShopButton.visible = true
+			$CombineButton.visible = true
 		GameState.GameStep.FIGHT:
-			$VBoxContainer/ShopButton.visible = false
-			$VBoxContainer/CombineButton.visible = false
-			$VBoxContainer/FightButton.visible = true
-
-func _on_ShopButton_pressed() -> void:
-	# Load the Shop scene
-	tutorial_popup.hide()
-	_load_scene("res://Scenes/Board/board.tscn")
-
-func _on_CombineButton_pressed() -> void:
-	# Load the Combine scene
-	_load_scene("res://Scenes/combine_board/combineboard.tscn")
-
-func _on_FightButton_pressed() -> void:
-	# Load the Fight scene
-	_load_scene("res://Scenes/forest_board/forestboard.tscn")
+			$ShopButton.visible = false
+			$CombineButton.visible = false
+			$FightButton.visible = true
 
 func _load_scene(scene_path: String) -> void:
 	print("Changing scene to:", scene_path)
 	GameState.update_step()  # Centralized game step update
 	get_tree().change_scene_to_file(scene_path)
 	_update_ui()
+
+
+func _on_fight_button_pressed() -> void:
+	# Load the Fight scene
+	_load_scene("res://Scenes/forest_board/forestboard.tscn")
+
+
+func _on_combine_button_pressed() -> void:
+	# Load the Combine scene
+	_load_scene("res://Scenes/combine_board/combineboard.tscn")
+
+
+func _on_shop_button_pressed() -> void:
+	# Load the Shop scene
+	_load_scene("res://Scenes/Board/board.tscn")
