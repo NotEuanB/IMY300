e474a258346e4d93eb15698b72497a03b9b3f7dd
Nolan <142321456+nolzftw@users.noreply.github.com>
2025-07-27 20:25:04 +0200

    Sync

--- IMY300 Main Game/new-game-project/Scenes/Board/fight_button.gd
+++ IMY300 Main Game/new-game-project/Scenes/Board/fight_button.gd
@@ -6,12 +6,5 @@ func _ready() -> void:
 	$FightSprite/FightAnimation.play("Fight_idle")
 
 func _on_pressed() -> void:
-	$FightSprite/FightAnimation.play("Fight_press")
 	emit_signal("fight_pressed")
 	$Button.play()
-
-func _on_mouse_entered() -> void:
-	$FightSprite/FightAnimation.play("Fight_hover")
-
-func _on_mouse_exited() -> void:
-	$FightSprite/FightAnimation.play("Fight_idle")
--- IMY300 Main Game/new-game-project/scenes/reroll_button/reroll_button.gd
+++ IMY300 Main Game/new-game-project/scenes/reroll_button/reroll_button.gd
@@ -21,12 +21,4 @@ func _on_player_stats_changed() -> void:
 
 func _on_pressed() -> void:
 	player_stats.gold -= 2
-	$HBoxContainer/Sprite2D/AnimationPlayer.play("Reroll_press")
 	$Reroll.play()
-	
-
-func _on_mouse_entered() -> void:
-	$HBoxContainer/Sprite2D/AnimationPlayer.play("Reroll_hover")
-
-func _on_mouse_exited() -> void:
-	$HBoxContainer/Sprite2D/AnimationPlayer.play("Reroll_idle")
