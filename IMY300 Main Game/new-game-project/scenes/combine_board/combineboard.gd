e79951d2e5ecd903ca7da110023ca4db38838314
NotEuanB <euanbotha@gmail.com>
2025-07-27 21:22:55 +0200

    Add tutorial popups and reward UI integration
    Introduced tutorial popups to the combine and forest boards, providing contextual guidance to players based on game steps. Added a reward UI scene with close functionality and connected it to the game flow. Updated GameState to include a new step (STEP_2_1) and adjusted step progression logic. Fixed resource paths for consistency and updated scene files to include new nodes and scripts.

--- IMY300 Main Game/new-game-project/scenes/combine_board/combineboard.gd
+++ IMY300 Main Game/new-game-project/scenes/combine_board/combineboard.gd
@@ -3,6 +3,9 @@ extends Node2D
 
 @onready var unit_mover: UnitMover = $UnitMover
 @onready var pause_menu = $PauseLayer/PauseMenu
+@onready var tutorial_popup = $TutorialPopup
+@onready var tutorial_text = $TutorialPopup/TutorialText
+
 var paused = false
 
 func _process(_delta: float) -> void:
@@ -19,12 +22,21 @@ func pauseMenu():
 	
 	paused = !paused
 
+
+func _show_tutorial_popup() -> void:
+	match GameState.current_step:
+		GameState.GameStep.STEP_2_1: 
+			tutorial_text.text = "This is the combine screen, where you can combine units to make a new one.\nBy looking at your units, you can see a yellow uncracked gem in the middle, which means it can be combined with another unit. Units cannot be combined with themself.\nDrag 2 different units into the slots and press the combine button to see the magic happen!"
+			tutorial_popup.position.x = 800
+			tutorial_popup.position.y = 500
+
 func _ready() -> void:
 	var states = GameState.load_state()
 	var board_units = states["board_units"]
 	var hand_units = states["hand_units"]
 	_spawn_units(board_units, $PlayArea)
 	_spawn_units(hand_units, $HandArea)
+	_show_tutorial_popup()
 
 func _spawn_units(units_data: Array, play_area: PlayArea) -> void:
 	for unit_data in units_data:
